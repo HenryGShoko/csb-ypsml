541e031a8c41f535c28b6e9a02bc9efae84fee04
Code Sandbox <hello@codesandbox.io>
2021-05-18 12:48:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9bc7ebbd5235ef08de84e4a8f7f62ae0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9544c13fc1eed018600a7dee9191ea",
+    "url": "https://henrygshoko.github.io/csb-ypsml/index.html"
+  },
+  {
+    "revision": "7729e391c2c76f6862a0",
+    "url": "https://henrygshoko.github.io/csb-ypsml/static/js/2.f5b8c434.chunk.js"
+  },
+  {
+    "revision": "2ca0fbc31ea9a191eb34",
+    "url": "https://henrygshoko.github.io/csb-ypsml/static/js/main.40273eed.chunk.js"
+  },
+  {
+    "revision": "1592aa7f8bc09ee13dcd",
+    "url": "https://henrygshoko.github.io/csb-ypsml/static/js/runtime-main.666c9229.js"
+  }
+]);
